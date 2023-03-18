3183f6a690644b88625b57f9903cd0e28fe65dec
SholomoCode <worksolom5@gmail.com>
2023-03-18 10:26:29 -0500

    task 3 complete

--- TechJobsOOAutoGraded6/CoreCompetency.cs
+++ TechJobsOOAutoGraded6/CoreCompetency.cs
@@ -3,16 +3,16 @@ namespace TechJobsOOAutoGraded6
 {
 	public class CoreCompetency
 	{
-        private int Id { get; set; }
-        private static int NextId = 1;
-        private string Value { get; set; }
+        public int Id { get; set; }
+        public static int nextId = 1;
+        public string Value { get; set; }
 
         // TODO: Task 2: Change the fields to auto-implemented properties.
 
         public CoreCompetency()
         {
-            Id = NextId;
-            NextId++;
+            Id = nextId;
+            nextId++;
         }
 
         public CoreCompetency(string v) : this()
